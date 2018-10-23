55f9725ab4570a080f941c73933991911ab547f7
Imanol <imanolroncomerino@gmail.com>
2018-10-23 09:06:39 +0200

    Adding UserBean.

---
+++ src/model/UserPrivilege.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ * This enumeration is for users profiles. Includes the values USER and ADMIN.
+ * @author Leticia
+ */
+public enum UserPrivilege {
+    
+    /**
+     * The user is a regular user.
+     */
+    USER,
+    /**
+     * The user is a privileged user.
+     */
+    ADMIN;
+}
---
+++ src/model/UserStatus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *This enumeration is for users status. Includes the values ENABLED and DISABLED.
+ * @author Leticia
+ */
+public enum UserStatus {
+     /**
+     * If user is in date base is ENABLED.
+     */
+    ENABLED,
+    /**
+     *If user isn't in date base is DISABLED  .
+     */
+    DISABLED;
+}
